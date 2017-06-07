e28445d5b8165b20225648d58a89112818c6f9ac
Efficiency97 <Efficiency97@163.com>
2017-06-07 19:13:52 +0800

    last

--- js/address.js
+++ js/address.js
@@ -1,23 +1,47 @@
 new Vue({
-  el: '.container',
-  data: {
-    addressList: []
-  },
-  mounted: function() {
-    this.$nextTick(function() {
-      this.getAddressList();
-    });
-  },
-  methods: {
-    getAddressList: function() {
-      var _this = this;
-      this.$http.get("data/address.json").then(function(response) {
-        var res = response.data;
-        if (res.status = "0") {
-          _this.addressList = res.result;
-        }
-      });
+   el:'.container' ,
+   //data中有所有vue'的模型，通过模型操作页面元素、Dom
+   data:{
+       limitNum:3,
+       addressList:[],
+       currentIndex: 0,
+       shippingMethod:1
+   },
+   mounted:function () {
+    this.$nextTick(function () {
+        this.getAddressList();
+    })
+   },
+    computed:{
+      filterAddressList:function () {
+          return this.addressList.slice(0,this.limitNum);
+      }
     },
+    //定义所有事件以及我们要调用的方法
+    methods:{
+      getAddressList:function () {
+          var _this=this;
+          this.$http.get('data/address.json').then(function (response) {
+              var res=response.data;
+              if(res.status ='0'){
+                _this.addressList=res.result;
+              }
+          })
+      },
+      loadMore:function () {
+          var _this=this;
+          if(this.limitNum==3){
+              _this.limitNum=_this.addressList.length;
+          }
+      },
+      setDefault:function (addressId) {
+          this.addressList.forEach(function (address,index) {
+              if(address.addressId==addressId){
+                  address.isDefault=true;
+              }else{
+                  address.isDefault=false;
+              }
+          })
+      }
     }
-  }
 });
--- js/cart.js
+++ js/cart.js
@@ -90,66 +90,8 @@ new Vue({
        this.productList.splice(index,1);
        this.delFlag=false;
       }
-      // changeMoney: function(product, way) {
-      //   if (way > 0) {
-      //     product.productQuantity++;
-      //   }
-      //   else {
-      //     product.productQuantity--;
-      //     if (product.productQuantity < 1) {
-      //       product.productQuantity = 1;
-      //     }
-      //   }
-      //   this.calcTotalPrice();
-      //
-      // },
-      // selectedProduct: function(item) {
-      //   if (typeof item.checked == 'undefined') {
-      //     // Vue.set(item, "checked", true);
-      //     this.$set(item, "checked", true);
-      //   }
-      //   else {
-      //     item.checked = !item.checked;
-      //   }
-      //   this.calcTotalPrice();
-      // },
-      // checkAll: function(flag) {
-      //   this.checkAllFlag = flag;
-      //   var _this = this;
-      //   this.productList.forEach(function (item, index) {
-      //     if (typeof item.checked == 'undefined', _this.checkAllFlag) {
-      //       _this.$set(item, "checked", _this.checkAllFlag);
-      //     }
-      //     else {
-      //       item.checked = _this.checkAllFlag;
-      //     }
-      //   });
-      //   this.calcTotalPrice();
-      // },
-      // calcTotalPrice: function() {
-      //   var _this = this;
-      //   this.totalMoney = 0;
-      //   this.productList.forEach(function(item, index) {
-      //     if (item.checked) {
-      //       _this.totalMoney += item.productPrice * item.productQuantity;
-      //     }
-      //   });
-      // },
-      // delConfirm: function(item) {
-      //   this.delFlag = true;
-      //   this.curProduct = item;
-      // },
-      // delProduct: function() {
-      //   var index = this.productList.indexOf(this.curProduct);
-      //   this.productList.splice(index, 1);
-      //   this.delFlag = false;
-      // }
     }
 });
-// // 全局过滤器
-// Vue.filter('money', function(value, type) {
-//   return "¥" + value.toFixed(2) + type;
-// })
 Vue.filter("money",function (value,type) {
     return "￥"+value.toFixed(2)+type
 })
